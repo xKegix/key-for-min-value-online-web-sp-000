275d0018720eaf37360143c51a002084c87745ed
Keginold Philip <keginoldp@hotmail.com>
2021-01-29 16:10:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,12 @@
 
 def key_for_min_value(name_hash)
 	low_value = 0
-	low_key = 0
+	low_key = nil
 
 	name_hash.each do |key,value|
 		if low_value == 0 || value < low_value
 			low_value = value
 			low_key = key
-		else
-			key == 0 || value == 0
-			nil
 end
 end
 low_key
